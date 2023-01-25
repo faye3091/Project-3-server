167416a08506068a060a9d74295944c74b0ddadf
Vanessa <faye_3091@yahoo.com>
2023-01-24 21:52:14 -0500

    added github action to deploy to heroku

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const PORT = 3001;
+const app = express();
+app.get('/', (req,res) => {
+    res.json({message: 'Hello World'})
+})
+
+app.listen(PORT, () => console.log(`express listening on port ${PORT}`));
